bb9008790aa32680179826d63490fc889ba7829a
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-10-10 13:26:31 +0530

    custom validation added

--- home/serializers.py
+++ home/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from  .models import *
 
 
-class StudentSerializer (serializers.ModelSerializer):
+class StudentSerializer  (serializers.ModelSerializer):
     class Meta:
         model= Student
         exclude= ['id']
